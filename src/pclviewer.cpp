d0c1e42ba59b3081fe2497f532619b66da53c200
Sven Röttering <svenroettering@googlemail.com>
2014-12-18 12:35:43 +0100

    Modified GUI

--- src/pclviewer.cpp
+++ src/pclviewer.cpp
@@ -3,12 +3,10 @@
 
 PCLViewer::PCLViewer (QWidget *parent) :
     QMainWindow (parent),
-    ui (new Ui::PCLViewer),
-    filtering_axis_ (1),  // = y
-    color_mode_ (4)  // = Rainbow
+    ui (new Ui::PCLViewer)
 {
   ui->setupUi (this);
-  this->setWindowTitle ("PCL viewer");
+  this->setWindowTitle ("Desktracking");
 
   // Setup the cloud pointer
   cloud_.reset (new PointCloudT);
@@ -23,11 +21,6 @@ PCLViewer::PCLViewer (QWidget *parent) :
   // Connect "Load" and "Save" buttons and their functions
   connect (ui->pushButton_load, SIGNAL(clicked ()), this, SLOT(loadFileButtonPressed ()));
   connect (ui->pushButton_save, SIGNAL(clicked ()), this, SLOT(saveFileButtonPressed ()));
-
-  // Connect X,Y,Z radio buttons and their functions
-  connect (ui->radioButton_x, SIGNAL(clicked ()), this, SLOT(axisChosen ()));
-  connect (ui->radioButton_y, SIGNAL(clicked ()), this, SLOT(axisChosen ()));
-  connect (ui->radioButton_z, SIGNAL(clicked ()), this, SLOT(axisChosen ()));
 
   connect (ui->StartReceiverButton, SIGNAL(clicked()) , this, SLOT(on_StartReceiverButton_clicked()));
 
@@ -108,149 +101,6 @@ PCLViewer::saveFileButtonPressed ()
   {
     PCL_ERROR("Error writing point cloud %s\n", filename.toStdString ().c_str ());
     return;
-  }
-}
-
-void
-PCLViewer::axisChosen ()
-{
-  // Only 1 of the button can be checked at the time (mutual exclusivity) in a group of radio buttons
-  if (ui->radioButton_x->isChecked ())
-  {
-    PCL_INFO("x filtering chosen\n");
-    filtering_axis_ = 0;
-  }
-  else if (ui->radioButton_y->isChecked ())
-  {
-    PCL_INFO("y filtering chosen\n");
-    filtering_axis_ = 1;
-  }
-  else
-  {
-    PCL_INFO("z filtering chosen\n");
-    filtering_axis_ = 2;
-  }
-
-  colorCloudDistances ();
-  viewer_->updatePointCloud (cloud_, "cloud");
-  ui->qvtkWidget->update ();
-}
-
-void
-PCLViewer::colorCloudDistances ()
-{
-  // Find the minimum and maximum values along the selected axis
-  double min, max;
-  // Set an initial value
-  switch (filtering_axis_)
-  {
-    case 0:  // x
-      min = cloud_->points[0].x;
-      max = cloud_->points[0].x;
-      break;
-    case 1:  // y
-      min = cloud_->points[0].y;
-      max = cloud_->points[0].y;
-      break;
-    default:  // z
-      min = cloud_->points[0].z;
-      max = cloud_->points[0].z;
-      break;
-  }
-
-  // Search for the minimum/maximum
-  for (PointCloudT::iterator cloud_it = cloud_->begin (); cloud_it != cloud_->end (); ++cloud_it)
-  {
-    switch (filtering_axis_)
-    {
-      case 0:  // x
-        if (min > cloud_it->x)
-          min = cloud_it->x;
-
-        if (max < cloud_it->x)
-          max = cloud_it->x;
-        break;
-      case 1:  // y
-        if (min > cloud_it->y)
-          min = cloud_it->y;
-
-        if (max < cloud_it->y)
-          max = cloud_it->y;
-        break;
-      default:  // z
-        if (min > cloud_it->z)
-          min = cloud_it->z;
-
-        if (max < cloud_it->z)
-          max = cloud_it->z;
-        break;
-    }
-  }
-
-  // Compute LUT scaling to fit the full histogram spectrum
-  double lut_scale = 255.0 / (max - min);  // max is 255, min is 0
-
-  if (min == max)  // In case the cloud is flat on the chosen direction (x,y or z)
-    lut_scale = 1.0;  // Avoid rounding error in boost
-
-  for (PointCloudT::iterator cloud_it = cloud_->begin (); cloud_it != cloud_->end (); ++cloud_it)
-  {
-    int value;
-    switch (filtering_axis_)
-    {
-      case 0:  // x
-        value = boost::math::iround ( (cloud_it->x - min) * lut_scale);  // Round the number to the closest integer
-        break;
-      case 1:  // y
-        value = boost::math::iround ( (cloud_it->y - min) * lut_scale);
-        break;
-      default:  // z
-        value = boost::math::iround ( (cloud_it->z - min) * lut_scale);
-        break;
-    }
-
-    // Apply color to the cloud
-    switch (color_mode_)
-    {
-      case 0:
-        // Blue (= min) -> Red (= max)
-        cloud_it->r = value;
-        cloud_it->g = 0;
-        cloud_it->b = 255 - value;
-        break;
-      case 1:
-        // Green (= min) -> Magenta (= max)
-        cloud_it->r = value;
-        cloud_it->g = 255 - value;
-        cloud_it->b = value;
-        break;
-      case 2:
-        // White (= min) -> Red (= max)
-        cloud_it->r = 255;
-        cloud_it->g = 255 - value;
-        cloud_it->b = 255 - value;
-        break;
-      case 3:
-        // Grey (< 128) / Red (> 128)
-        if (value > 128)
-        {
-          cloud_it->r = 255;
-          cloud_it->g = 0;
-          cloud_it->b = 0;
-        }
-        else
-        {
-          cloud_it->r = 128;
-          cloud_it->g = 128;
-          cloud_it->b = 128;
-        }
-        break;
-      default:
-        // Blue -> Green -> Red (~ rainbow)
-        cloud_it->r = value > 128 ? (value - 128) * 2 : 0;  // r[128] = 0, r[255] = 255
-        cloud_it->g = value < 128 ? 2 * value : 255 - ( (value - 128) * 2);  // g[0] = 0, g[128] = 255, g[255] = 0
-        cloud_it->b = value < 128 ? 255 - (2 * value) : 0;  // b[0] = 255, b[128] = 0
-    }
   }
 }
 
--- src/pclviewer.h
+++ src/pclviewer.h
@@ -21,7 +21,7 @@
 
 #include "KinectFrameReceiver.h"
 
-typedef pcl::PointXYZRGBA PointT;
+typedef pcl::PointXYZ PointT; //RGBA PointT;
 typedef pcl::PointCloud<PointT> PointCloudT;
 
 namespace Ui
@@ -50,38 +50,12 @@ class PCLViewer : public QMainWindow
     void
     loadFileButtonPressed ();
 
-    /** @brief Triggered whenever a button in the "Color on axis" group is clicked */
-    void
-    axisChosen ();
-
   protected:
     /** @brief The PCL visualizer object */
     boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer_;
 
     /** @brief The point cloud displayed */
     PointCloudT::Ptr cloud_;
-
-    /** @brief 0 = x | 1 = y | 2 = z */
-    int filtering_axis_;
-
-    /** @brief Holds the color mode for @ref colorCloudDistances */
-    int color_mode_;
-
-    /** @brief Color point cloud on X,Y or Z axis using a Look-Up Table (LUT)
-     * Computes a LUT and color the cloud accordingly, available color palettes are :
-     *
-     *  Values are on a scale from 0 to 255:
-     *  0. Blue (= 0) -> Red (= 255), this is the default value
-     *  1. Green (= 0) -> Magenta (= 255)
-     *  2. White (= 0) -> Red (= 255)
-     *  3. Grey (< 128) / Red (> 128)
-     *  4. Blue -> Green -> Red (~ rainbow)
-     *
-     * @warning If there's an outlier in the data the color may seem uniform because of this outlier!
-     * @note A boost rounding exception error will be thrown if used with a non dense point cloud
-     */
-    void
-    colorCloudDistances ();
 
 private slots:
     void on_StartReceiverButton_clicked();
